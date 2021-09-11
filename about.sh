aeebad9e48ac0f0addbb59dda577f8aa839c5c33
racunzx <88345813+racunzx@users.noreply.github.com>
2021-09-11 13:59:22 +0800

    Add files via upload

---
+++ about.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+clear
+echo -e "================================================="
+echo -e "#     Premium Auto Script By Brody           #" 
+echo -e "#-----------------------------------------------#" 
+echo -e "# For Debian 9 & Debian 10 64 bit               #" 
+echo -e "# For Ubuntu 18.04 & Ubuntu 20.04 64 bit        #" 
+echo -e "# For VPS with KVM and VMWare virtualization    #" 
+echo -e "#-----------------------------------------------#" 
+echo -e "# Thanks To  :                                  #" 
+echo -e "#-----------------------------------------------#" 
+echo -e "# ALLAH S.W.T                                   #" 
+echo -e "# My Family                                     #" 
+echo -e "# Google                                        #" 
+echo -e "# Admin Portal & White Vps                      #" 
+echo -e "#                                               #" 
+echo -e "#-----------------------------------------------#" 
+echo -e "# Rebuild By : BRODY                        #" 
+echo -e "=================================================" 
